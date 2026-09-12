47c21a7b77efcc4f85ffdd0cf6c4a8f31f8e29cd
shivuu773 <shivyadav18405@gmail.com>
2026-09-12 13:34:22 +0530

    feat: added OrchestratorApiApplication main class and application.properties

---
+++ orchestrator-api/src/main/java/com/orchestrator/api/OrchestratorApiApplication.java
@@ -0,0 +1,13 @@
+package com.orchestrator.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EntityScan(basePackages = "com.orchestrator.core.domain") 
+public class OrchestratorApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrchestratorApiApplication.class, args);
+    }
+}
